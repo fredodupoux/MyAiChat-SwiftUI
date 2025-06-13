a819196b5b72f4dba29e2517e993a09f9933501c
Frederic <fredodupoux@gmail.com>
2025-06-13 16:46:45 -0400

    Initial Commit

---
+++ MyAiChat/MyAiChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAiChatApp.swift
+//  MyAiChat
+//
+//  Created by Frederic Dupoux on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MyAiChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
